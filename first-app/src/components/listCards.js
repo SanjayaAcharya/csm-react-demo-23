3921474fb92a8acd41c29fd5a0b8384e01ccc824
Himalaya <himalaya.pagada@csm.co.in>
2023-05-29 15:44:23 +0530

    ok

--- first-app/src/components/listCards.js
+++ first-app/src/components/listCards.js
@@ -5,10 +5,10 @@ function CardListComponent() {
     const [listObj, setListObj] = useState([])
     
     useEffect(() => {        
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        fetch('https://dummyjson.com/products')
         .then(res => res.json())
         .then(json => {
-            setListObj(JSON.parse(JSON.stringify(json)));
+            setListObj(JSON.parse(JSON.stringify(json.products)));
         })
     }, [])
 
@@ -20,9 +20,10 @@ function CardListComponent() {
                     listObj.map((item, i) => {
                         return(
                             <div className="card mr-2 mb-2" key={i} style={{maxWidth: '32%'}}>
+                                <img className="card-img-top" src={item.thumbnail} alt="profile" />
                                 <div className="card-body">
                                     <h5 className="card-title">{item.title}</h5>
-                                    <p className="card-text">{item.body}</p>
+                                    <p className="card-text">{item.description}</p>
                                 </div>
                             </div>
                         )
